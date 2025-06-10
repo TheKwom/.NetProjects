4de8cf50ac2c126525794bda08282cc2aa4fdf0c
TheKwom <dafwendz@gmail.com>
2025-06-10 11:35:22 -0600

    added search/filter

--- SyringaHelpDesk/obj/Debug/net8.0/SyringaHelpDesk.AssemblyInfo.cs
+++ SyringaHelpDesk/obj/Debug/net8.0/SyringaHelpDesk.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SyringaHelpDesk")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8a81f4e1dcbddc4317df6d20d49bc6ad9229d66")]
 [assembly: System.Reflection.AssemblyProductAttribute("SyringaHelpDesk")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SyringaHelpDesk")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
